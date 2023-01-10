08f64d289c768660e20898d28dad698ff53cbaa6
yonas <yonasleykun27@gmail.com>
2023-01-10 12:37:52 -0800

    all tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
